9ebeb664aa81dda15f8b10749896a376bf8cf5d6
RafaelAlejandro20 <82453734+RafaelAlejandro20@users.noreply.github.com>
2021-06-11 01:44:47 -0500

    Add files via upload

---
+++ DerivadaConFormula2RafaelAlejandroBarajasUrrutia3202.m
@@ -0,0 +1,18 @@
+% Titulo: Derivada Con Formula General.
+% Descripcion: Script para graficar una funcion de la forma f (x) = +2 .
+% Autor: Rafael Alejandro Barajas Urrutia.
+% Fecha: 15 de abril de 2021.
+
+% limpiar variables
+ clear
+% Ejemplo f (x) = a
+% Rango de -3..3 en i = 0.1
+ x = [-5: 1 :5]; 
+% Valor de la función 
+ y=((10-(x)))
+% Dibujar x, y
+ plot(x , y);
+% Titulo ("f (x)=y=((10-(x)))
+ 
+% Etiqueta para x
+ xlabel ( "x");
---
+++ DerivadaConFormulaGeneralRafaelAlejandroBarajasUrrutia3202.m
@@ -0,0 +1,20 @@
+%Titulo : Derivada con formula general.
+% Descripción: Script para graficar la funcion.
+% Autor: Barajas Urrutia Rafael Alejandro.
+% Fecha: 15 de abril de 2021.
+
+% limpiar variables
+clear
+% ejemplo
+%rango de -5..5 en i=1
+x = -2 : 4 : 5 ;
+% Valor de la función
+f = (16-x.^2)/(4)-x;
+% Dibujar x, y
+  plot(f,-x);
+% Titulo
+title ( "x = 16 -x^2 / 4-x" );
+% Etiqueta para x
+xlabel ( " x " );
+% Etiqueta para y
+ylabel ( " f(x) " );
